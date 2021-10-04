bc1a9dc67db6037e480d7a1c23850015e6f0ff0f
jaics <jai.cs@somaiya.edu>
2021-10-04 13:41:38 +0530

    Sending data complete except images part

--- Files/newsletter/faculty_panel/models.py
+++ Files/newsletter/faculty_panel/models.py
@@ -6,3 +6,31 @@ class Highlights(models.Model):
     faculty_name = models.CharField(max_length=100)
     achievement = models.TextField()
     image = models.ImageField(upload_to='images')
+
+class Milestones(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+class Activities(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    caption = models.CharField(max_length=200)
+
+class Placements(models.Model):
+    company = models.CharField(max_length=200)
+    number = models.IntegerField()
+
+class Students(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+class Events(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+class Projects(models.Model):
+    description = models.TextField()
+
+class Phd(models.Model):
+    description = models.TextField()
+
--- Files/newsletter/faculty_panel/views.py
+++ Files/newsletter/faculty_panel/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from faculty_panel.models import Highlights
+from faculty_panel.models import *
 from django.contrib import messages
 
 # Create your views here.
@@ -27,30 +27,63 @@ def faculty_highlights(request, num):
         achievement = request.POST['achievements'+str(x)]
         image = request.POST['high-img'+str(x)]
         if faculty_name != '' or achievement != '' or image != '':
-            if image in request.FILES: print("image there")
             faculty_highlights = Highlights(faculty_name = faculty_name, achievement = achievement, image=image)
             faculty_highlights.save()
 
 def remarkable_milestones(request, num):
-    pass
+    for x in range (int(num[1])+1):
+        description = request.POST['milestones-desc'+str(x)]
+        image = request.POST['milestones-img'+str(x)]
+        if description != '' or image != '':
+            remarkable_milestones = Milestones(description=description, image=image)
+            remarkable_milestones.save()
 
 def activities_conducted(request, num):
-    pass
+    for x in range (int(num[2])+1):
+        description = request.POST['activitiess-desc'+str(x)]
+        image = request.POST['activities-img'+str(x)]
+        caption = request.POST['activities-cap'+str(x)]
+        if description != '' or image != '' or caption != '':
+            activities_conducted = Activities(description=description, image=image, caption=caption)
+            activities_conducted.save()
 
 def placement_stats(request, num):
-    pass
+    for x in range(int(num[3])+1):
+        company = request.POST['placements-comp'+str(x)]
+        number = request.POST['placements-num'+str(x)]
+        if company != '' or number != '':
+            placement_stats = Placements(company=company, number=number)
+            placement_stats.save()
 
 def student_achievements(request, num):
-    pass
+    for x in range (int(num[4])+1):
+        description = request.POST['students-desc'+str(x)]
+        image = request.POST['students-img'+str(x)]
+        if description != '' or image != '':
+            student_achievements = Students(description=description, image=image)
+            student_achievements.save()
 
 def events_faculties(request, num):
-    pass
+    for x in range (int(num[5])+1):
+        description = request.POST['events-desc'+str(x)]
+        image = request.POST['events-img'+str(x)]
+        if description != '' or image != '':
+            events_faculties = Events(description=description, image=image)
+            events_faculties.save()
 
 def project_companies(request, num):
-    pass
+    for x in range (int(num[6])+1):
+        description = request.POST['projects-desc'+str(x)]
+        if description != '':
+            project_companies = Projects(description=description)
+            project_companies.save()
 
 def phd_faculties(request, num):
-    pass
+    for x in range (int(num[7])+1):
+        description = request.POST['phd-desc'+str(x)]
+        if description != '':
+            phd_faculties = Phd(description=description)
+            phd_faculties.save()
 
 
 
