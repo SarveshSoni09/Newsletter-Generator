c8d91acdc2e13f8312ae0dedd6cdcb63e5d820b6
jaics <jai.cs@somaiya.edu>
2021-10-23 13:29:40 +0530

    Documentation reports updated

--- Files/newsletter/login/views.py
+++ Files/newsletter/login/views.py
@@ -9,27 +9,27 @@ from admin_panel.views import get_data
 
 def login(request):
     all_data = get_data()
-    return render(request, 'admin-panel.html', {'all_data' : all_data})
+    # return render(request, 'admin-panel.html', {'all_data' : all_data})
 
-    # if request.method == 'POST':
-    #     email = request.POST['email']
-    #     pwd = request.POST['pwd']
-    #     user = auth.authenticate(username=email, password=pwd)
-    #     if user is not None:
-    #         if user.is_superuser:
-    #             auth.login(request, user)
-    #             # return redirect('admin-panel')
-    #             return render(request, 'admin-panel.html', {'all_data' : all_data})
-    #         else:
-    #             auth.login(request, user)
-    #             # return redirect('faculty-panel')
-    #             return render(request, 'faculty-panel.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        pwd = request.POST['pwd']
+        user = auth.authenticate(username=email, password=pwd)
+        if user is not None:
+            if user.is_superuser:
+                auth.login(request, user)
+                # return redirect('admin-panel')
+                return render(request, 'admin-panel.html', {'all_data' : all_data})
+            else:
+                auth.login(request, user)
+                # return redirect('faculty-panel')
+                return render(request, 'faculty-panel.html')
                 
-    #     else:
-    #         messages.info(request, 'Invalid email or password')
-    #         return redirect('/')
-    # else:
-    #     return render(request, 'index.html')
+        else:
+            messages.info(request, 'Invalid email or password')
+            return redirect('/')
+    else:
+        return render(request, 'index.html')
 
 def test(request):
     return render(request, 'test.html')
